2ed580cb98ce334b2e573ac762ccbe6880ec4609
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-17 01:13:45 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,28 +2,60 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "",
+    lastName: "",
+    submittedData: []
   }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
   }
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
+  // handleFirstNameChange = event => {
+  //   this.setState({
+  //     firstName: event.target.value
+  //   })
+  // }
+
+  // handleLastNameChange = event => {
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let formData = { 
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+    this.setState({ firstName: "", lastName: ""})
+    //this.sendFormDataSomewhere(formData)
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+    return <div>
+            <span>{data.firstName} </span> 
+            <span>{data.lastName}</span>
+          </div>
     })
   }
 
   render() {
+    console.log(this.state)
+    console.log(this.listOfSubmissions())
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form onSubmit={e => this.handleSubmit(e) }>
+          <input type="text" name="firstName" onChange={event => this.changeHandler(event)} value={this.state.firstName} />
+          <input type="text" name="lastName" onChange={event => this.changeHandler(event)} value={this.state.lastName} />
+          <input type="submit"/>
+        </form>
+        {this.listOfSubmissions()}
+      </div>
     )
   }
 }
